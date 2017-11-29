725dc94728cff6d05fa250ef9fdd0b43234aa0fa
Keisuke Kan <9renpoto@gmail.com>
2017-11-29 20:27:08 +0900

    Updates

--- lib/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export declare type Row = {
-    name: string;
-    point: number;
-    time: string;
-};
-export default function (props: {
-    data: Array<Row>;
-}): JSX.Element;
