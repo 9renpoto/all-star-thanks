1deb1ac7ccea453c93efda50d508129387b69067
Keisuke Kan <9renpoto@gmail.com>
2019-09-22 10:12:43 +0900

    :thinking:

--- stories/index.jsx
+++ stories/index.jsx
@@ -1,17 +1,16 @@
 /* @flow */
-import { storiesOf } from '@storybook/react'
-import { date, random } from 'faker'
-import React from 'react'
+import { storiesOf } from "@storybook/react";
+import { date, random } from "faker";
+import React from "react";
+import "../src/css/style.css";
+import App from "../src/index.jsx";
 
-import '../src/css/style.css'
-import App from '../src'
-
-storiesOf('Content', module).add('App', () => (
+storiesOf("Content", module).add("App", () => (
   <App
     data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => ({
       name: random.word(),
       point: random.number({ max: 999 }),
-      time: `${new Intl.DateTimeFormat('en-US').format(date.past())}`
+      time: `${new Intl.DateTimeFormat("en-US").format(date.past())}`
     }))}
   />
-))
+));
