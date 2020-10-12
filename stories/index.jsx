a60559e8112e51d614b9556147b7d968f8f6a6bb
Keisuke Kan <9renpoto@gmail.com>
2020-10-13 00:05:28 +0900

    refactor: use CSF format

--- stories/index.jsx
+++ stories/index.jsx
@@ -1,16 +1,20 @@
 /* @flow */
-import { storiesOf } from "@storybook/react";
 import { date, random } from "faker";
 import React from "react";
 import "../src/css/style.css";
 import App from "../src/index.jsx";
 
-storiesOf("Content", module).add("App", () => (
+export default {
+  title: "Content",
+  component: App,
+};
+
+export const _App = () => (
   <App
     data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => ({
       name: random.word(),
       point: random.number({ max: 999 }),
-      time: `${new Intl.DateTimeFormat("en-US").format(date.past())}`
+      time: `${new Intl.DateTimeFormat("en-US").format(date.past())}`,
     }))}
   />
-));
+);
