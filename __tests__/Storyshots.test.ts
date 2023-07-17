29b6232b27aadf62ecdf420285f7e57dcdfe5c49
Keisuke Umeno <9renpoto@gmail.com>
2023-07-17 15:23:21 +0900

    chore: use push

--- __tests__/Storyshots.test.ts
+++
@@ -1,8 +0,0 @@
-import initStoryshots from '@storybook/addon-storyshots';
-const { advanceTo } = require("jest-date-mock");
-
-advanceTo(new Date(2018, 5, 27, 0, 0, 0));
-
-initStoryshots({
-  framework: "preact",
-});
