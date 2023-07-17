6da02822c1acf47b044c642d4f8ac2f8b13b83a9
Keisuke Umeno <9renpoto@gmail.com>
2023-07-17 15:25:48 +0900

    chore: format

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
 export type Row = {
-  name: string,
-  point: number,
-  time: string,
-  rank?: number
-}
+  name: string;
+  point: number;
+  time: string;
+  rank?: number;
+};
 
 export default function (props: { data: Array<Row> }) {
   return (
     <ul>
       {props.data.map((d, i) => (
         <li key={`thanks-${i}`}>
-          <span className='num'>{d.rank ? d.rank : 10 - i}</span>
-          <span className='name'>{d.name}</span>
-          <span className='point'>
+          <span className="num">{d.rank ? d.rank : 10 - i}</span>
+          <span className="name">{d.name}</span>
+          <span className="point">
             {d.point}
             <small>P</small>
           </span>
-          <span className='time'>{d.time}</span>
+          <span className="time">{d.time}</span>
         </li>
       ))}
     </ul>
-  )
+  );
 }
